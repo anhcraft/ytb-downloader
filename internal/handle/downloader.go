ab12b8f46bf443e073e5cffb55e662ca49f758e9
anhcraft <huynhduyanh123123@gmail.com>
2023-11-27 11:15:51 +0700

    Support concurrent downloading

--- internal/handle/downloader.go
+++ internal/handle/downloader.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"ytb-downloader/internal/format"
 	"ytb-downloader/internal/settings"
 )
@@ -43,103 +44,123 @@ func Download(onUpdate func(progress float64), onError func(err error), onFinish
 
 func _download(onUpdate func(progress float64), onFinish func(), onError func(err error), processing []*Process) {
 	totalProgress := float64(len(processing) * 100)
-	progress := float64(0)
+	var progress atomic.Int32
+	var semaphore sync.WaitGroup
+	semaphore.Add(len(processing))
+	jobs := make(chan *Process, len(processing))
 
-	for _, v := range processing {
-		func() {
-			var err error
-			defer func() {
-				if err != nil {
-					v.Status = Error
-					onError(err)
-				}
-			}()
+	for i := 0; i < settings.Get().ConcurrentDownloads; i++ {
+		go func() {
+			for job := range jobs {
+				func() {
+					var err error
+					defer func() {
+						if err != nil {
+							job.Status = Error
+							onError(err)
+						}
+						semaphore.Done()
+					}()
 
-			log.Printf("downloading %s\n", v.URL)
-			v.Status = Downloading
-			onUpdate(progress / totalProgress)
+					log.Printf("downloading %s\n", job.URL)
+					job.Status = Downloading
+					onUpdate(float64(progress.Load()) / totalProgress)
 
-			args := []string{"--ignore-errors", "--no-warnings",
-				"--progress", "--newline",
-				//"--progress-template", "%(progress)j",
-				"--concurrent-fragments", strconv.Itoa(settings.Get().ConcurrentDownloads),
-				"--abort-on-unavailable-fragments",
-				"-P", settings.Get().GetDownloadFolder(),
-				"--embed-thumbnail", "--embed-metadata"}
+					args := []string{"--ignore-errors", "--no-warnings",
+						"--progress", "--newline",
+						//"--progress-template", "%(progress)j",
+						"--concurrent-fragments", strconv.Itoa(settings.Get().ConcurrentFragments),
+						"--abort-on-unavailable-fragments",
+						"-P", settings.Get().GetDownloadFolder(),
+						"--embed-thumbnail", "--embed-metadata"}
 
-			if fp := settings.Get().GetFFmpegPath(); fp != "" {
-				args = append(args, "--ffmpeg-location", fp)
-			}
-
-			if v.Format == format.VideoOnly {
-				args = append(args, "-f", "bestvideo")
-			} else if v.Format == format.AudioOnly {
-				args = append(args, "-f", "bestaudio")
-			}
-
-			args = append(args, v.URL)
-
-			cmd := exec.Command("./yt-dlp.exe", args...)
-			log.Printf("Executing command %s\n", cmd.String())
-
-			stdout, err1 := cmd.StdoutPipe()
-			if err1 != nil {
-				log.Println("Error creating StdoutPipe:", err1)
-				err = err1
-				onUpdate(progress / totalProgress)
-				return
-			}
-
-			stderr, err1 := cmd.StderrPipe()
-			if err1 != nil {
-				log.Println("Error creating StderrPipe:", err1)
-				err = err1
-				onUpdate(progress / totalProgress)
-				return
-			}
-
-			if err1 = cmd.Start(); err1 != nil {
-				log.Println("Error starting command:", err1)
-				err = err1
-				onUpdate(progress / totalProgress)
-				return
-			}
-
-			reader := bufio.NewReader(io.MultiReader(stdout, stderr))
-
-			for {
-				line, err1 := reader.ReadString('\n')
-				if err1 != nil {
-					if err1 == io.EOF {
-						break
+					if fp := settings.Get().GetFFmpegPath(); fp != "" {
+						args = append(args, "--ffmpeg-location", fp)
 					}
-					log.Println("Error reading line:", err1)
-					break
-				}
 
-				// analyze log
-				line = strings.TrimSpace(line)
-				if p, ok := extractPercentage(line); ok {
-					onUpdate((progress + p) / totalProgress)
-				} else if strings.Contains(line, "ffmpeg not found") {
-					err = errors.New("error due to ffmpeg not installed")
-				} else {
-					log.Println(line)
-				}
+					// Choose the best quality format
+					// Remux the video to mp4 or audio to m4a to support thumbnail embedding
+					if job.Format == format.VideoOnly {
+						args = append(args, "-f", "bestvideo", "--remux-video", "mp4")
+					} else if job.Format == format.AudioOnly {
+						args = append(args, "-f", "bestaudio", "--remux-video", "m4a")
+					} else {
+						args = append(args, "--remux-video", "mp4")
+					}
+
+					args = append(args, job.URL)
+
+					cmd := exec.Command("./yt-dlp.exe", args...)
+					log.Printf("Executing command %s\n", cmd.String())
+
+					stdout, err1 := cmd.StdoutPipe()
+					if err1 != nil {
+						log.Println("Error creating StdoutPipe:", err1)
+						err = err1
+						onUpdate(float64(progress.Load()) / totalProgress)
+						return
+					}
+
+					stderr, err1 := cmd.StderrPipe()
+					if err1 != nil {
+						log.Println("Error creating StderrPipe:", err1)
+						err = err1
+						onUpdate(float64(progress.Load()) / totalProgress)
+						return
+					}
+
+					if err1 = cmd.Start(); err1 != nil {
+						log.Println("Error starting command:", err1)
+						err = err1
+						onUpdate(float64(progress.Load()) / totalProgress)
+						return
+					}
+
+					reader := bufio.NewReader(io.MultiReader(stdout, stderr))
+					lastPercentage := int32(0)
+
+					for {
+						line, err1 := reader.ReadString('\n')
+						if err1 != nil {
+							if err1 == io.EOF {
+								break
+							}
+							log.Println("Error reading line:", err1)
+							break
+						}
+
+						// analyze log
+						line = strings.TrimSpace(line)
+						if p, ok := extractPercentage(line); ok {
+							deltaPercentage := int32(p) - lastPercentage
+							lastPercentage = int32(p)
+							onUpdate(float64(progress.Add(deltaPercentage)) / totalProgress)
+						} else if strings.Contains(line, "ffmpeg not found") {
+							err = errors.New("error due to ffmpeg not installed")
+						} else {
+							log.Println(line)
+						}
+					}
+
+					if err1 = cmd.Wait(); err1 != nil && err == nil {
+						log.Println("Error on running command:", err1)
+						err = err1
+						onUpdate(float64(progress.Load()) / totalProgress)
+						return
+					}
+
+					job.Status = Done
+					onUpdate(float64(progress.Load()) / totalProgress)
+				}()
 			}
-
-			if err1 = cmd.Wait(); err1 != nil && err == nil {
-				log.Println("Error on running command:", err1)
-				err = err1
-				onUpdate(progress / totalProgress)
-				return
-			}
-
-			v.Status = Done
-			progress += 100
-			onUpdate(progress / totalProgress)
 		}()
 	}
 
+	for _, v := range processing {
+		jobs <- v
+	}
+	close(jobs)
+
+	semaphore.Wait()
 	onFinish()
 }
