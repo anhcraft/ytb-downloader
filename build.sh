4d7d481c62ac208b3284b612465db437db8d193b
anhcraft <huynhduyanh123123@gmail.com>
2025-02-21 14:04:56 +0700

    Update build script

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 os=$(uname -s)
+version="1.0.8"
 
 case "$os" in
     Darwin)
-      fyne package -os darwin -icon "./assets/ytb.png"
+      fyne package -os darwin -icon "./assets/ytb.png" -name "YTB Downloader" -appVersion "$version"
         ;;
     Linux)
-      fyne package -os linux -icon "./assets/ytb.png"
+      fyne package -os linux -icon "./assets/ytb.png" -name "YTB Downloader" -appVersion "$version"
         ;;
     MINGW*) # Windows Git bash
-      fyne package -os windows -icon "./assets/ytb.png"
+      fyne package -os windows -icon "./assets/ytb.png" -name "YTB Downloader" -appVersion "$version" -executable "YTB Downloader.exe"
         ;;
     *)
         echo "Unsupported operating system."
